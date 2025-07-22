3d8f145c4aad87871d776a8800911deeb5fe1c81
2hangz <187074978+2hangz@users.noreply.github.com>
2025-07-22 16:18:20 +0100

    fix: add dotenv dependency for render

--- routes/articles.js
+++ routes/articles.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const Article = require('../models/Article');
+const Article = require('../models/Articles');
 
 const router = express.Router();
 
