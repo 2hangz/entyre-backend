d96bca1f05fa77322f71dc6cc354786de8f8ab0c
2hangz <187074978+2hangz@users.noreply.github.com>
2025-07-22 16:21:26 +0100

    fix: mongoDB

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
