2010bbf106fb9c83c3e309eb7f8aebcae3ad2494
2hangz <187074978+2hangz@users.noreply.github.com>
2025-09-27 11:03:24 +0800

    bunch files uploaded

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 // middleware/auth.js
 const { verifyToken } = require('../utils/jwt');
-const User = require('../models/User');
+const User = require('../models/login');
 
 /**
  * JWT authentication middleware.
